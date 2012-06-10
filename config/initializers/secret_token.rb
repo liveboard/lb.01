a690a1eb4a579a7a8b8eb83fd562dc5b4eda483c
liveboard <alexander.paul.stein@gmail.com>
2012-06-10 14:14:47 -0700

    Bare

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liveboard01::Application.config.secret_token = '23dbb133caf589c4873efdaea9ff5ed5baa0463c198529a14f0075c8e92f1b2ad3d3042bef5776b77250f74f1c950f3e2be8f47b3402422c63e102d9ff7f7cda'
